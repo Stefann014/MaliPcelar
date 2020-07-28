2a46e8c57e0ab85eea8df199152c70e3da4eaecc
Stefann014 <stefanmarjanovic1997@gmail.com>
2020-07-28 20:12:23 +0200

    lecenje pocetak

--- app/src/main/java/com/example/malipcelar/activity/activity/Dodaj_IzmeniLecenjeActivity.java
+++ app/src/main/java/com/example/malipcelar/activity/activity/Dodaj_IzmeniLecenjeActivity.java
@@ -1,22 +1,33 @@
 package com.example.malipcelar.activity.activity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.DatePicker;
 import android.widget.Spinner;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 import com.example.malipcelar.R;
+import com.example.malipcelar.activity.fragmenti.DatumPickerFragment;
 
+import java.text.DateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
-public class Dodaj_IzmeniLecenjeActivity extends AppCompatActivity {
+public class Dodaj_IzmeniLecenjeActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
 
-    Button btnDatum;
+    Button btnDatumLecenja;
     Spinner spBolesti;
     CheckBox chPrimeniLecenjeNaPcelinjak;
 
@@ -29,10 +40,12 @@ public class Dodaj_IzmeniLecenjeActivity extends AppCompatActivity {
     }
 
     private void srediAtribute() {
-        btnDatum = findViewById(R.id.btnDatumLecenja);
+        btnDatumLecenja = findViewById(R.id.btnDatumLecenja);
         spBolesti = findViewById(R.id.spBolesti);
         chPrimeniLecenjeNaPcelinjak = findViewById(R.id.chPrimeniLecenjeNaPcelinjak);
         srediSpinner();
+        srediDatum();
+        srediListenere();
     }
 
     private void srediSpinner() {
@@ -55,6 +68,65 @@ public class Dodaj_IzmeniLecenjeActivity extends AppCompatActivity {
             public void onNothingSelected(AdapterView<?> parent) {
             }
         });
+    }
+
+
+    private void srediDatum() {
+        Date c = Calendar.getInstance().getTime();
+        String currentDateString = DateFormat.getDateInstance().format(c.getTime());
+        btnDatumLecenja.setText(currentDateString);
+    }
+
+    private void srediListenere() {
+        btnDatumLecenja.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment datePicker = new DatumPickerFragment();
+                datePicker.show(getSupportFragmentManager(), "date picker");
+            }
+        });
+
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.YEAR, year);
+        c.set(Calendar.MONTH, month);
+        c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+        String currentDateString = DateFormat.getDateInstance().format(c.getTime());
+        btnDatumLecenja.setText(currentDateString);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.dodaj_novu_napomenu_meni, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.sacuvaj_napomenu:
+                sacuvajLecenje();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void sacuvajLecenje() {
+    }
+
+    @NonNull
+    private String prevediDatumUFormatZaBazu(String datum) {
+        //treba nam format yyyy-MM-dd
+        datum = datum.substring(0, datum.length() - 1);
+        datum = datum.replace('.', '-');
+        String[] datumi = datum.split("-");
+        String dobarDatum = datumi[2] + "-" + datumi[1] + "-" + datumi[0];
+        return dobarDatum;
     }
 
 }
