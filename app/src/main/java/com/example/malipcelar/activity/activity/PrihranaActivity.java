42e01b3317da709934949d7f9ebd161423ad513c
Stefann014 <stefanmarjanovic1997@gmail.com>
2020-07-29 13:15:46 +0200

    prihrana 2

--- app/src/main/java/com/example/malipcelar/activity/activity/PrihranaActivity.java
+++ app/src/main/java/com/example/malipcelar/activity/activity/PrihranaActivity.java
@@ -2,14 +2,17 @@ package com.example.malipcelar.activity.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.malipcelar.R;
+import com.example.malipcelar.activity.fragmenti.BottomSheetDialog;
 import com.example.malipcelar.activity.domen.Kosnica;
+import com.example.malipcelar.activity.domen.Pcelinjak;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class PrihranaActivity extends AppCompatActivity {
+public class PrihranaActivity extends AppCompatActivity implements BottomSheetDialog.BottomSheetListener {
 
     public static final String EXTRA_KOSNICA =
             "com.example.malipcelar.activity.activity.KOSNICA";
@@ -17,6 +20,8 @@ public class PrihranaActivity extends AppCompatActivity {
             "com.example.malipcelar.activity.activity.PCELINJAK";
 
     Kosnica kosnica;
+    Pcelinjak pcelinjak;
+    FloatingActionButton btnDodajNovuPrihranu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +29,31 @@ public class PrihranaActivity extends AppCompatActivity {
         setContentView(R.layout.prihrana_activity);
 
         srediAtribute();
-        Log.d("TAG", "" + kosnica);
+        srediListenere();
+    }
+
+    private void srediListenere() {
+        btnDodajNovuPrihranu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BottomSheetDialog bottomSheet = new BottomSheetDialog();
+                bottomSheet.show(getSupportFragmentManager(), "exampleBottomSheet");
+            }
+        });
+    }
+
+    @Override
+    public void onBtnPogacaClicked() {
+    }
+
+    @Override
+    public void onBtnSirupClicked() {
     }
 
     private void srediAtribute() {
         Intent intent = getIntent();
         kosnica = (Kosnica) intent.getSerializableExtra(EXTRA_KOSNICA);
+        pcelinjak = (Pcelinjak) intent.getSerializableExtra(EXTRA_PCELINJAK);
+        btnDodajNovuPrihranu = findViewById(R.id.btnDodajNovuPrihranu);
     }
 }
