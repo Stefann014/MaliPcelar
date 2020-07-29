ace0fb9d4ea5adab42afbc07a1d089c4f8381952
Stefann014 <stefanmarjanovic1997@gmail.com>
2020-07-29 14:51:53 +0200

    prihrana 3

--- app/src/main/java/com/example/malipcelar/activity/activity/PrihranaActivity.java
+++ app/src/main/java/com/example/malipcelar/activity/activity/PrihranaActivity.java
@@ -2,17 +2,19 @@ package com.example.malipcelar.activity.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.malipcelar.R;
-import com.example.malipcelar.activity.fragmenti.BottomSheetDialog;
+import com.example.malipcelar.activity.ExampleDialog;
 import com.example.malipcelar.activity.domen.Kosnica;
 import com.example.malipcelar.activity.domen.Pcelinjak;
+import com.example.malipcelar.activity.fragmenti.BottomSheetDialog;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class PrihranaActivity extends AppCompatActivity implements BottomSheetDialog.BottomSheetListener {
+public class PrihranaActivity extends AppCompatActivity implements BottomSheetDialog.BottomSheetListener, ExampleDialog.ExampleDialogListener {
 
     public static final String EXTRA_KOSNICA =
             "com.example.malipcelar.activity.activity.KOSNICA";
@@ -44,6 +46,8 @@ public class PrihranaActivity extends AppCompatActivity implements BottomSheetDi
 
     @Override
     public void onBtnPogacaClicked() {
+        ExampleDialog exampleDialog = new ExampleDialog();
+        exampleDialog.show(getSupportFragmentManager(), "example dialog");
     }
 
     @Override
@@ -56,4 +60,12 @@ public class PrihranaActivity extends AppCompatActivity implements BottomSheetDi
         pcelinjak = (Pcelinjak) intent.getSerializableExtra(EXTRA_PCELINJAK);
         btnDodajNovuPrihranu = findViewById(R.id.btnDodajNovuPrihranu);
     }
+
+    @Override
+    public void sacuvaj(String datum, String kg) {
+
+
+
+    }
+
 }
