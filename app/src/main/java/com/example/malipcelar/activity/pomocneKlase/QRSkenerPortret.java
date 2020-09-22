aeb320deb91c7753b2cf423b862e9bcd3e5685b0
Stefann014 <stefanmarjanovic1997@gmail.com>
2020-09-22 19:38:48 +0200

    Ubacivanje QR koda

---
+++ app/src/main/java/com/example/malipcelar/activity/pomocneKlase/QRSkenerPortret.java
@@ -0,0 +1,7 @@
+package com.example.malipcelar.activity.pomocneKlase;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class QRSkenerPortret extends CaptureActivity {
+    // sluzi samo da se okrene skener qr koda nna portret
+}
